cecb89ef85ce631da07b90b22522055056b3520b
André Marvell Ikounga <marvell.ikounga@gmail.com>
2015-07-10 16:43:43 +0200

    Changes : Formulaire de connexion et de registration

--- src/Application/Sonata/UserBundle/Controller/ProfileFOSUser1Controller.php
+++ src/Application/Sonata/UserBundle/Controller/ProfileFOSUser1Controller.php
@@ -66,32 +66,4 @@ class ProfileFOSUser1Controller extends BaseController
     }
 
 
-//    /**
-//     * @return Response
-//     *
-//     * @throws AccessDeniedException
-//     */
-//    public function editProfileAction()
-//    {
-//        $user = $this->container->get('security.context')->getToken()->getUser();
-//        if (!is_object($user) || !$user instanceof UserInterface) {
-//            throw new AccessDeniedException('This user does not have access to this section.');
-//        }
-//
-//        $form = $this->container->get('sonata.user.profile.form');
-//        $formHandler = $this->container->get('sonata.user.profile.form.handler');
-//
-//        $process = $formHandler->process($user);
-//        if ($process) {
-//            $this->setFlash('sonata_user_success', 'profile.flash.updated');
-//
-//            return new RedirectResponse($this->generateUrl('sonata_user_profile_show'));
-//        }
-//
-//        return $this->render('SonataUserBundle:Profile:edit_profile.html.twig', array(
-//            'form'               => $form->createView(),
-//            'breadcrumb_context' => 'user_profile',
-//        ));
-//    }
-
 }
