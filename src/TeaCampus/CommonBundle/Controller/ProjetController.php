7fab73715c22db401aa6bdb307ae05330ff26db7
SAMIE MEHEZA <meheza.samie@utt.fr>
2015-07-12 15:30:09 +0200

    Change: Message , NavBAr  - Mise à jour de la messagerie  - Mise à jour de la NavBar pour l'afficahe des projets poplaires et des articles les plus lues

--- src/TeaCampus/CommonBundle/Controller/ProjetController.php
+++ src/TeaCampus/CommonBundle/Controller/ProjetController.php
@@ -8,198 +8,177 @@ use TeaCampus\CommonBundle\Form\ProjetType;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\Request;
 
-class ProjetController extends Controller
-{
-    public function indexAction()
-    {
-        $em                  = $this->getDoctrine()->getManager();
-        $projetRepo          = $em->getRepository('TeaCampusCommonBundle:Projet');
-        $idselectedProjet    = $projetRepo->getSelectedProjet();
+class ProjetController extends Controller {
+
+    public function indexAction() {
+        $em = $this->getDoctrine()->getManager();
+        $projetRepo = $em->getRepository('TeaCampusCommonBundle:Projet');
+        $idselectedProjet = $projetRepo->getSelectedProjet();
         $idProjectOfTheMonth = $projetRepo->getProjectOfTheMonth();
-        $idProjectOfTheWeek  = $projetRepo->getProjectOfTheWeek();
-        $tags                = $em->getRepository('ApplicationSonataClassificationBundle:Tag')->findByContext('projet');
-        $mostRead       = $em->getRepository('TeaCampusCommonBundle:Projet')->findMostRead();
-        
-        $selectedProjet      = $projetRepo->findOneById($idselectedProjet);
-        $projetofthemonth    = $projetRepo->findOneById($idProjectOfTheMonth);
-        $projetoftheweek     = $projetRepo->findOneById($idProjectOfTheWeek);
-        
-        $query               = $em->createQuery(
-                                    'SELECT p FROM TeaCampusCommonBundle:Projet p
+        $idProjectOfTheWeek = $projetRepo->getProjectOfTheWeek();
+        $tags = $em->getRepository('ApplicationSonataClassificationBundle:Tag')->findByContext('projet');
+        $mostRead = $em->getRepository('TeaCampusCommonBundle:Projet')->findMostRead();
+        $AmostRead = $em->getRepository('ApplicationSonataNewsBundle:Post')->findMostRead();
+        $selectedProjet = $projetRepo->findOneById($idselectedProjet);
+        $projetofthemonth = $projetRepo->findOneById($idProjectOfTheMonth);
+        $projetoftheweek = $projetRepo->findOneById($idProjectOfTheWeek);
+
+        $query = $em->createQuery(
+                'SELECT p FROM TeaCampusCommonBundle:Projet p
                                     WHERE p.private = false
                                     AND p.enabled = true
                                     ORDER BY p.date DESC'
-                                    );
+        );
         $query->setMaxResults(4);
         $latestProject = $query->getResult();
-        
-        
-        
-        return $this->render('TeaCampusCommonBundle:Projet:index.html.twig',
-                            array('selectionOftea'=>$selectedProjet,
-                                  'projectofthemonth'=>$projetofthemonth,
-                                  'latestproject' =>$latestProject,
-                                  'tags' =>$tags,
-                                  'mostRead' => $mostRead,
-                                  'projectoftheweek'=>$projetoftheweek));
-        
-        
+
+
+
+        return $this->render('TeaCampusCommonBundle:Projet:index.html.twig', array('selectionOftea' => $selectedProjet,
+                    'projectofthemonth' => $projetofthemonth,
+                    'latestproject' => $latestProject,
+                    'tags' => $tags,
+                    'AmostRead' => $AmostRead,
+                    'mostRead' => $mostRead,
+                    'projectoftheweek' => $projetoftheweek));
     }
-    public function addAction()
-    {
-        
-        $em         = $this->getDoctrine()->getManager();
-        $context    = $em->getRepository('ApplicationSonataClassificationBundle:Context')->find('projet');
-        $form       = $this->createForm(new ProjetType($em, $context), new Projet());
-        
-        $form->handleRequest($this->getRequest());        
+
+    public function addAction() {
+
+        $em = $this->getDoctrine()->getManager();
+        $context = $em->getRepository('ApplicationSonataClassificationBundle:Context')->find('projet');
+        $form = $this->createForm(new ProjetType($em, $context), new Projet());
+
+        $form->handleRequest($this->getRequest());
         if ($form->isValid()) {
-            
-            $projet     = $form->getData();
+
+            $projet = $form->getData();
             // On recupere le current user
-            $usr        = $this->get('security.context')->getToken()->getUser();
+            $usr = $this->get('security.context')->getToken()->getUser();
             $projet->setAuthor($usr);
-            
-            $em->persist($projet);
-            $em->flush();  
-            
-            $projet->createThread();
-                        
+
             $em->persist($projet);
             $em->flush();
-            
+
+            $projet->createThread();
+
+            $em->persist($projet);
+            $em->flush();
+
 //            \Doctrine\Common\Util\Debug::dump($projet->getMedia());
 //            exit;
-            
-            
+
+
             $html = $this->container->get('templating')->render('TeaCampusCommonBundle:Projet:profile_list_item.html.twig', array('projet' => $projet));
             $response = new Response();
-            $response->setContent(json_encode(array("success"=>true,"content"=>$html)));
-            $response -> headers -> set('Content-Type', 'application/json');
-            
+            $response->setContent(json_encode(array("success" => true, "content" => $html)));
+            $response->headers->set('Content-Type', 'application/json');
+
             return $response;
-            
-            
         }
-        
+
         return $this->render('TeaCampusCommonBundle:Projet:add.html.twig', array('form' => $form->createView()));
-        
     }
-    
-    public function deleteAction()
-    {
-        $request    = $this->get('request');
-        $em         = $this->getDoctrine()->getManager();
-        $id         = $request->request->get('id');
+
+    public function deleteAction() {
+        $request = $this->get('request');
+        $em = $this->getDoctrine()->getManager();
+        $id = $request->request->get('id');
         $repository = $em->getRepository("TeaCampusCommonBundle:Projet");
-        $projet     = $repository->find($id);
-        
-        $user= $this->get('security.context')->getToken()->getUser();
-        
+        $projet = $repository->find($id);
+
+        $user = $this->get('security.context')->getToken()->getUser();
+
         $success = false;
-        if(is_null($projet) || $user->getId()!==$projet->getAuthor()->getId()){
-                throw new NotFoundHttpException("Désolé, la page que vous avez demandée semble introuvable !");
-        }else{
+        if (is_null($projet) || $user->getId() !== $projet->getAuthor()->getId()) {
+            throw new NotFoundHttpException("Désolé, la page que vous avez demandée semble introuvable !");
+        } else {
             $success = true;
             $em->remove($projet);
             $em->flush();
-            
         }
-        
+
         $response = new Response();
-        $response->setContent(json_encode(array("success"=>$success)));
-        $response -> headers -> set('Content-Type', 'application/json');
+        $response->setContent(json_encode(array("success" => $success)));
+        $response->headers->set('Content-Type', 'application/json');
 
         return $response;
-        
     }
-    
-   public function listAction(Request $request)
-    {
-        $em                  = $this->getDoctrine()->getManager();
-        $projetRepo          = $em->getRepository('TeaCampusCommonBundle:Projet');
-        $tags                = $em->getRepository('ApplicationSonataClassificationBundle:Tag')->findByContext('projet');
-        $mostRead            = $em->getRepository('TeaCampusCommonBundle:Projet')->findMostRead();
-        $dql        = "SELECT a FROM TeaCampusCommonBundle:Projet a WHERE a.private = false AND a.enabled = true ORDER BY a.date DESC";
-        $query      = $em->createQuery($dql);
-        
-        $paginator  = $this->get('knp_paginator');
+
+    public function listAction(Request $request) {
+        $em = $this->getDoctrine()->getManager();
+        $projetRepo = $em->getRepository('TeaCampusCommonBundle:Projet');
+        $tags = $em->getRepository('ApplicationSonataClassificationBundle:Tag')->findByContext('projet');
+        $mostRead = $em->getRepository('TeaCampusCommonBundle:Projet')->findMostRead();
+        $dql = "SELECT a FROM TeaCampusCommonBundle:Projet a WHERE a.private = false AND a.enabled = true ORDER BY a.date DESC";
+        $query = $em->createQuery($dql);
+        $paginator = $this->get('knp_paginator');
         $pagination = $paginator->paginate(
                 $query, $request->query->get('page', 1)/* page number */, 8/* limit per page */
         );
-        
-        
-        return $this->render('TeaCampusCommonBundle:Projet:list.html.twig',
-                            array(
-                                  'tags' =>$tags,
-                                  'mostRead' => $mostRead,
-                                  'pagination' => $pagination));
-         
+
+
+        return $this->render('TeaCampusCommonBundle:Projet:list.html.twig', array(
+                    'tags' => $tags,
+                    'mostRead' => $mostRead,
+                    'pagination' => $pagination));
     }
-   
-    public function searchAction()
-    {
+
+    public function searchAction() {
         return $this->render('TeaCampusCommonBundle:Projet:list.html.twig');
-         
     }
-    
-     public function showAction($id)
-    {
-        $em              = $this->getDoctrine()->getManager();
-        $projetRepo      = $em->getRepository('TeaCampusCommonBundle:Projet');
-        $projet          = $projetRepo->findOneById($id);
-        
-        if(is_null($projet)){
+
+    public function showAction($id) {
+        $em = $this->getDoctrine()->getManager();
+        $projetRepo = $em->getRepository('TeaCampusCommonBundle:Projet');
+        $projet = $projetRepo->findOneById($id);
+
+        if (is_null($projet)) {
             throw new NotFoundHttpException("Désolé, la page que vous avez demandée semble introuvable !");
-        }else{
-            $query               = $em->createQuery(
-                                   'SELECT p FROM TeaCampusCommonBundle:Projet p
+        } else {
+            $query = $em->createQuery(
+                    'SELECT p FROM TeaCampusCommonBundle:Projet p
                                     WHERE p.private = false
                                     AND p.enabled = true
                                     ORDER BY p.date DESC'
-                                    );
-            
+            );
+
             $randomProject = $query->getResult();
             shuffle($randomProject);
 
             $newArray = array_splice($randomProject, 0, 3);
 
-            return $this->render('TeaCampusCommonBundle:Projet:show.html.twig',
-                                array('project'=>$projet,
-                                      'randomproject'=>$newArray));
+            return $this->render('TeaCampusCommonBundle:Projet:show.html.twig', array('project' => $projet,
+                        'randomproject' => $newArray));
         }
-         
     }
-    
-    public function editAction($id)
-    {
-        
-        $em         = $this->getDoctrine()->getManager();
-        $context    = $em->getRepository('ApplicationSonataClassificationBundle:Context')->find('projet');
-        $project    = $em->getRepository('TeaCampusCommonBundle:Projet')->find($id);
-        
-        $project    = (is_null($project))? new Projet() : $project;
-        $form       = $this->createForm(new ProjetType($em, $context), $project);
-        
-        $form->handleRequest($this->getRequest());        
+
+    public function editAction($id) {
+
+        $em = $this->getDoctrine()->getManager();
+        $context = $em->getRepository('ApplicationSonataClassificationBundle:Context')->find('projet');
+        $project = $em->getRepository('TeaCampusCommonBundle:Projet')->find($id);
+
+        $project = (is_null($project)) ? new Projet() : $project;
+        $form = $this->createForm(new ProjetType($em, $context), $project);
+
+        $form->handleRequest($this->getRequest());
         if ($form->isValid()) {
-            
-            $projet     = $form->getData();
-                        
+
+            $projet = $form->getData();
+
             $em->persist($projet);
             $em->flush();
-            
+
             $html = $this->container->get('templating')->render('TeaCampusCommonBundle:Projet:profile_list_item.html.twig', array('projet' => $projet));
             $response = new Response();
-            $response->setContent(json_encode(array("success"=>true,"content"=>$html)));
-            $response -> headers -> set('Content-Type', 'application/json');
-            
+            $response->setContent(json_encode(array("success" => true, "content" => $html)));
+            $response->headers->set('Content-Type', 'application/json');
+
             return $response;
-            
-            
         }
-        
+
         return $this->render('TeaCampusCommonBundle:Projet:edit.html.twig', array('form' => $form->createView()));
-        
     }
+
 }
