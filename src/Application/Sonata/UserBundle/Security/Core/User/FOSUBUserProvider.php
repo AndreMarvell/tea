77a33842c86b97175d2c4ec533984e483f9b2738
André Marvell Ikounga <marvell.ikounga@gmail.com>
2015-07-15 02:28:01 +0200

    Bugs : Connexion par Gmail

--- src/Application/Sonata/UserBundle/Security/Core/User/FOSUBUserProvider.php
+++ src/Application/Sonata/UserBundle/Security/Core/User/FOSUBUserProvider.php
@@ -53,47 +53,58 @@ class FOSUBUserProvider extends BaseClass
      */
     public function loadUserByOAuthUserResponse(UserResponseInterface $response)
     {
+        if ($response->getResponse() == null) {
+          throw new \Exception('Couldn\'t retrieve the API response of : '. $response->getResourceOwner()->getName());
+        }
         $username   = $response->getUsername();
         
         $user = $this->userManager->findUserBy(array($this->getProperty($response) => $username));
+        
         //when the user is registrating
         if (null === $user) {
-            $service = $response->getResourceOwner()->getName();
-            $setter = 'set'.ucfirst($service);
-            $setter_id = $setter.'Id';
-            $setter_token = $setter.'AccessToken';
-            // create new user here
-            $user = $this->userManager->createUser();
-            $user->$setter_id($username);
-            $user->$setter_token($response->getAccessToken());
-            //I have set all requested data with the user's username
-            //modify here with relevant data
-            $user->setUsername($username);
-            $user->setEmail($response->getEmail());
-            $user->setFirstName($response->getFirstName());
-            $user->setLastName($response->getLastName());
-            $user->setEmail($response->getEmail());
-            $user->setPasswordUpdated(false);
-            if($service=="google"){
-                $user->setAvatar($this->downloader->downloadFile(
-                        $response->getProfilePicture(),
-                        $response->getRealName(),
-                        'avatar',
-                        'google_'.$username.'.jpg'
-                    ));
-            }elseif ($service=="facebook") {
-                $user->setAvatar($this->downloader->downloadFile(
-                        'http://graph.facebook.com/'.$username.'/picture?type=large',
-                        $response->getRealName(),
-                        'avatar',
-                        'fb_'.$username.'.jpg'
-                    ));    
+            $user = $this->userManager->findUserBy(array('email' => $response->getEmail()));
+            if(null === $user){
+                $service = $response->getResourceOwner()->getName();
+                $setter = 'set'.ucfirst($service);
+                $setter_id = $setter.'Id';
+                $setter_token = $setter.'AccessToken';
+                // create new user here
+                $user = $this->userManager->createUser();
+                $user->$setter_id($username);
+                $user->$setter_token($response->getAccessToken());
+                //I have set all requested data with the user's username
+                //modify here with relevant data
+                
+                // Pour avoir un username stylé
+                $customUsername = explode("@",$response->getEmail())[0].ucfirst($service);
+                
+                $user->setUsername($customUsername);
+                $user->setEmail($response->getEmail());
+                $user->setFirstName($response->getFirstName());
+                $user->setLastName($response->getLastName());
+                $user->setEmail($response->getEmail());
+                $user->setPasswordUpdated(false);
+                if($service=="google"){
+                    $user->setAvatar($this->downloader->downloadFile(
+                            $response->getProfilePicture(),
+                            $response->getRealName(),
+                            'avatar',
+                            'google_'.$username.'.jpg'
+                        ));
+                }elseif ($service=="facebook") {
+                    $user->setAvatar($this->downloader->downloadFile(
+                            'http://graph.facebook.com/'.$username.'/picture?type=large',
+                            $response->getRealName(),
+                            'avatar',
+                            'fb_'.$username.'.jpg'
+                        ));    
+                }
+
+                $user->setPlainPassword($username);
+                $user->setEnabled(true);
+                $this->userManager->updateUser($user);
+                return $user;
             }
-            
-            $user->setPlainPassword($username);
-            $user->setEnabled(true);
-            $this->userManager->updateUser($user);
-            return $user;
         }
 
         //if user exists - go with the HWIOAuth way
