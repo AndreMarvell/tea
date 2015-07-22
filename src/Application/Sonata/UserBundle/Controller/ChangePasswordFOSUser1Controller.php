48fedaf3975a193e9fc3dfd655e7fe2406638e14
André Marvell Ikounga <marvell.ikounga@gmail.com>
2015-07-22 02:10:21 +0200

    Changes : Changement du mot de passe

--- src/Application/Sonata/UserBundle/Controller/ChangePasswordFOSUser1Controller.php
+++ src/Application/Sonata/UserBundle/Controller/ChangePasswordFOSUser1Controller.php
@@ -27,6 +27,7 @@ use Sonata\UserBundle\Controller\ChangePasswordFOSUser1Controller as BaseControl
  */
 class ChangePasswordFOSUser1Controller extends BaseController
 {
+    
     public function changePasswordAction()
     {
         $user = $this->container->get('security.context')->getToken()->getUser();
@@ -43,11 +44,28 @@ class ChangePasswordFOSUser1Controller extends BaseController
 
             return new RedirectResponse($this->getRedirectionUrl($user));
         }
-
+        
         return $this->container->get('templating')->renderResponse(
             'SonataUserBundle:ChangePassword:changePassword.html.'.$this->container->getParameter('fos_user.template.engine'),
             array('form' => $form->createView())
         );
+        
+    }
+    
+    public function changePasswordEditAuthenticationAction()
+    {
+        $user = $this->container->get('security.context')->getToken()->getUser();
+        if (!is_object($user) || !$user instanceof UserInterface) {
+            throw new AccessDeniedException('This user does not have access to this section.');
+        }
+
+        $form = $this->container->get('fos_user.change_password.form');
+
+        return $this->container->get('templating')->renderResponse(
+                'SonataUserBundle:ChangePassword:changePassword_content.html.'.$this->container->getParameter('fos_user.template.engine'),
+                array('form' => $form->createView())
+            ); 
+        
     }
 
     /**
